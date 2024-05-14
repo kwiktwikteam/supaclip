e9d75e97a4545d055df98cc34e6ab4375a37343f
bobde_yagyesh <yagyeshsrbobde@gmail.com>
2024-05-15 02:27:16 +0530

    udpated suspense

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import React, {useState, useEffect } from "react";
+import React, {useState, useEffect, Suspense } from "react";
 import Button from "~/components/ui/Button";
 // import { getVideoId } from "~/lib/helpers/other";
 import Image from "next/image";
@@ -17,7 +17,7 @@ const Page = () => {
   const [vidId, setvidId] = useState("")
   const { toast } = useToast();
 
-  const searchParams = useSearchParams();
+
 
   const handleSubmit = async () => {
     // router 
@@ -54,57 +54,69 @@ const Page = () => {
   }
 
 
-  useEffect(() => {
-    const q = searchParams.get("q");
-
-    if(q) {
-      setvideoUrl(q);
-    }
-  }, [])
+  
 
 
   return (
-    <div className="relative grid h-screen place-content-center gap-12 bg-black">
-      {/* <div className="absolute left-0 top-0 h-screen w-screen bg-black opacity-50"> */}
-      {/* </div> */}
-      <Image
-        src={"/hero_bg.png"}
-        alt="hero"
-        fill
-        priority
-        className="absolute z-10 h-full w-full opacity-30"
-      />
-      <Image
-        src={"/images/hero/bg.png"}
-        alt="hero"
-        fill
-        className="absolute z-10 h-full w-full opacity-50"
-      />
-      {/* <VidGallery
+    <Suspense>
+      <SetUrlToSearch seturl={setvideoUrl} />
+      <div className="relative grid h-screen place-content-center gap-12 bg-black">
+        {/* <div className="absolute left-0 top-0 h-screen w-screen bg-black opacity-50"> */}
+        {/* </div> */}
+        <Image
+          src={"/hero_bg.png"}
+          alt="hero"
+          fill
+          priority
+          className="absolute z-10 h-full w-full opacity-30"
+        />
+        <Image
+          src={"/images/hero/bg.png"}
+          alt="hero"
+          fill
+          className="absolute z-10 h-full w-full opacity-50"
+        />
+        {/* <VidGallery
         videos={themeVideos}
         className="z-20 gap-x-32"
         hideDetails={true}
       /> */}
-      <div className="absolute left-1/2 right-1/2 top-5 z-10 flex w-[90%] -translate-x-1/2 items-center justify-between px-5 *:text-4xl *:text-white md:w-3/4">
-        <Link href="/" className="border-2 rounded-full p-2">
-          <HomeIcon />
-        </Link>
+        <div className="absolute left-1/2 right-1/2 top-5 z-10 flex w-[90%] -translate-x-1/2 items-center justify-between px-5 *:text-4xl *:text-white md:w-3/4">
+          <Link href="/" className="rounded-full border-2 p-2">
+            <HomeIcon />
+          </Link>
+        </div>
+        <div className="flex-col-center-center z-10 gap-5">
+          <input
+            type="text"
+            placeholder="YouTube Video Id"
+            value={videoUrl}
+            className="w-[450px] rounded-full px-4 py-3"
+            onChange={(e) => setvideoUrl(e.target.value)}
+          />
+          <Button className="bg-white" onClick={handleSubmit}>
+            {/* <Button className='bg-white'  onClick={handleSubmit}> */}
+            Generate
+          </Button>
+        </div>
       </div>
-      <div className="flex-col-center-center z-10 gap-5">
-        <input
-          type="text"
-          placeholder="YouTube Video Id"
-          value={videoUrl}
-          className="w-[450px] rounded-full px-4 py-3"
-          onChange={(e) => setvideoUrl(e.target.value)}
-        />
-        <Button className="bg-white" onClick={handleSubmit}>
-          {/* <Button className='bg-white'  onClick={handleSubmit}> */}
-          Generate
-        </Button>
-      </div>
-    </div>
+    </Suspense>
   );
 };
+
+
+const SetUrlToSearch = ({ seturl } : {
+  seturl : (url: string) => void
+}) => {
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    const q = searchParams.get("q");
+
+    if (q) {
+      seturl(q);
+    }
+  }, []);
+  return (<></>)
+}
 
 export default Page;
